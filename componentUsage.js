23db87515299a27e2d1632852808958b344379fb
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-12-09 18:13:49 -0800

    Add chalk and error messaging / handling

--- componentUsage.js
+++ componentUsage.js
@@ -1,6 +1,7 @@
 const acorn = require("acorn/dist/acorn_loose");
 const walk = require('acorn/dist/walk');
 const fs = require('fs-extra');
+const chalk = require('chalk');
 const findIt = require('findit');
 const path = require('path');
 let myArgs = require('optimist').argv,
@@ -9,8 +10,14 @@ let compList = [];
 const searchPackages = ['react-redux', 'redux-modules', 'reselect'];
 const appRoot = '../Procore/procore/wrench/src';
 
-if ((myArgs.h) || (myArgs.help)) {
-	process.exit(0);
+if ( myArgs._.length === 0) {
+  console.log('No config file found, please specify a config file');
+  console.log('');
+  console.log(`Be sure to specify the following keys in your config file:
+   - ${chalk.green('searchPackages')}
+   - ${chalk.green('appRoot')}
+  `);
+	process.exit(1);
 }
 
 // ---------------------------------------
@@ -62,22 +69,59 @@ const walkDirectory = directory => new Promise((resolve) => {
 
 const configFile = myArgs._[0];
 
-const configOptions = fs.readJsonSync(configFile)
-compList = initialize(configOptions.searchPackages);
+fs.stat(configFile, (err, stat) => {
+  if(err == null) {
+    const configOptions = fs.readJsonSync(configFile)
+    if (!configOptions.searchPackages) {
+      console.log('');
+      console.log(`${chalk.red('searchPackages')} was not defined in your config file,`);
+      console.log('');
+      console.log(`${chalk.blue(configFile)}:`);
+      console.log('```');
+      console.log(JSON.stringify(configOptions, null, 2));
+      console.log('```');
+      console.log('');
+      console.log(`please specify ${chalk.red('searchPackages')} this key with an array of package names as strings.`);
+      process.exit(1);
+    } else if (!configOptions.appRoot) {
+      console.log('');
+      console.log(`${chalk.red('appRoot')} was not defined in the following config file`);
+      console.log('');
+      console.log(`${chalk.blue(configFile)}:`);
+      console.log('```');
+      console.log(JSON.stringify(configOptions, null, 2));
+      console.log('```');
+      console.log('');
+      console.log(`please specify ${chalk.red('appRoot')} with a path to the root of the project you would like to search in.`);
+      process.exit(1);
+    } else {
+      compList = initialize(configOptions.searchPackages);
 
-const directories = [configOptions.appRoot];
+      const directories = [configOptions.appRoot];
 
-const dirWalkPromises = directories.map(walkDirectory);
-Promise.all(dirWalkPromises).then(() => {
-  if (!configOptions.showPaths) {
-    compList = compList.map(comp => {
-      return {
-        name: comp.name,
-        count: comp.count,
-      }
-    });
+      const dirWalkPromises = directories.map(walkDirectory);
+      Promise.all(dirWalkPromises).then(() => {
+        if (!configOptions.showPaths) {
+          compList = compList.map(comp => {
+            return {
+              name: comp.name,
+              count: comp.count,
+            }
+          });
+        }
+
+        const formattedData = JSON.stringify(compList, null, 2);
+        console.log(formattedData);
+      });
+    }
+  } else if(err.code == 'ENOENT') {
+    console.log(`${chalk.red(configFile)} does not exist, please specify correct path to config file`);
+    console.log('');
+    console.log(`Be sure to specify the following keys in your config file:
+   - ${chalk.green('searchPackages')}
+   - ${chalk.green('appRoot')}
+  `);
+  } else {
+    console.log('Some other error: ', err.code);
   }
-
-	const formattedData = JSON.stringify(compList, null, 2);
-  console.log(formattedData);
 });
